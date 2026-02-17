8e0891191c3c519f1ff63f2b2835aed0939e0680
Luis Gabriel Neves Tristão <luisgabrielnevest@gmail.com>
2026-02-17 13:35:12 -0300

    Estrutura que o professor mandou

---
+++ atividade_semana02.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void preencherVetorDecrescente(int *vetor, int tamanho) {
+    
+}
+
+void bubbleSort(int *vetor, int tamanho, int *comparacoes, int *movimentacoes) {
+    
+    
+}
+
+void selectionSort(int *vetor, int tamanho, int *comparacoes, int *movimentacoes) {
+    
+}
+
+void executarTeste(const char *algoritmo, void (*sortFunction)(int *, int, int *, int *), int tamanho) {
+    int *vetor = (int *)malloc(tamanho * sizeof(int));
+    int comparacoes, movimentacoes;
+    clock_t inicio, fim;
+    double tempo;
+    
+    preencherVetorDecrescente(vetor, tamanho);
+    inicio = clock();
+    sortFunction(vetor, tamanho, &comparacoes, &movimentacoes);
+    fim = clock();
+    tempo = ((double)(fim - inicio)) / (CLOCKS_PER_SEC / 1000);
+    
+    printf("| %-13s | %-7d | %-10.3f | %-12d | %-12d |\n", algoritmo, tamanho, tempo, comparacoes, movimentacoes);
+    free(vetor);
+}
+
+int main() {
+    printf("| Algoritmo     | Tamanho | Tempo (ms) | Comparacoes  | Movimentacoes  |\n");
+    printf("|---------------|---------|------------|--------------|----------------|\n");
+    executarTeste("Bubble Sort", bubbleSort, 100);
+    printf("|---------------|---------|------------|--------------|----------------|\n");
+    executarTeste("Selection Sort", selectionSort, 100);
+    printf("|---------------|---------|------------|--------------|----------------|\n");
+    executarTeste("Bubble Sort", bubbleSort, 1000);
+    printf("|---------------|---------|------------|--------------|----------------|\n");
+    executarTeste("Selection Sort", selectionSort, 1000);
+    printf("|---------------|---------|------------|--------------|----------------|\n");
+    executarTeste("Bubble Sort", bubbleSort, 10000);
+    printf("|---------------|---------|------------|--------------|----------------|\n");
+    executarTeste("Selection Sort", selectionSort, 10000);
+    printf("|---------------|---------|------------|--------------|----------------|\n");
+    return 0;
+}
